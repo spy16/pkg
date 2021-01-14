713a7da36fb79255d099cd6982f29849e782239a
Shivaprasad <shiv.ylp@gmail.com>
2021-01-14 20:00:28 +0530

    add Git prefix to git shell funcs

--- shell/git.go
+++ shell/git.go
@@ -5,8 +5,8 @@ import (
 	"path/filepath"
 )
 
-// Init initializes a git repo in the given directory.
-func Init(targetDir string, name string) error {
+// GitInit initializes a git repo in the given directory.
+func GitInit(targetDir string, name string) error {
 	target := filepath.Join(targetDir, name)
 	if err := os.MkdirAll(target, os.ModePerm); err != nil {
 		return err
@@ -15,15 +15,15 @@ func Init(targetDir string, name string) error {
 	return err
 }
 
-// Clone runs 'git clone' with the repoURL and clones the repo contents into
+// GitClone runs 'git clone' with the repoURL and clones the repo contents into
 // targetDir. targetDir must be an empty directory.
-func Clone(targetDir, repoURL string) error {
+func GitClone(targetDir, repoURL string) error {
 	_, err := gitExec(targetDir, "clone", repoURL, targetDir)
 	return err
 }
 
-// Commit runs `git commit -m message` and returns the output.
-func Commit(targetDir, message string) ([]byte, error) {
+// GitCommit runs `git commit -m message` and returns the output.
+func GitCommit(targetDir, message string) ([]byte, error) {
 	return gitExec(targetDir, "commit", "-m", message)
 }
 
